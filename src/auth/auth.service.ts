71aec8bb4f72c4ec756544f4d4b6ad841577ca57
Bilal Hanif <bilalhanif81@yahoo.com>
2024-09-25 19:33:36 +0400

    upload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../users/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
